237f9e51c21de5211a2c4dc31184945c93d19d39
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2025-12-07 18:54:41 +0000

    gameboard fimnished

--- src/components/ScratchLine/ScratchLine.jsx
+++ src/components/ScratchLine/ScratchLine.jsx
@@ -2,88 +2,96 @@ import "../ScratchLine/ScratchLine.css";
 
 export default function ScratchLine() {
     return (
-        <div id="scratch-line-container">
-            <div id="scratch-line">
-                <div className="letter"></div>
-                <div className="letter"></div>
-                <div className="letter">S</div>
-                <div className="letter">C</div>
-                <div className="letter">R</div>
-                <div className="letter">A</div>
-                <div className="letter">T</div>
-                <div className="letter">C</div>
-                <div className="letter">H</div>
-                <div className="letter"></div>
-                <div className="letter"></div>
+        <>
+            <div id="scratch-line-container">
+                <div id="scratch-line">
+                    <div className="letter"></div>
+                    <div className="letter"></div>
+                    <div className="letter">S</div>
+                    <div className="letter">C</div>
+                    <div className="letter">R</div>
+                    <div className="letter">A</div>
+                    <div className="letter">T</div>
+                    <div className="letter">C</div>
+                    <div className="letter">H</div>
+                    <div className="letter"></div>
+                    <div className="letter"></div>
+                </div>
+            </div> 
+            <div className="scratch-dots-container">
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
+                <div className="scratch-dot">
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                    <div className="dot"></div>
+                </div>
             </div>
-            <div id="scratch-dots-container">
-                <div className=" scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-                <div className="scratch-dots">
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                    <div classname="dot"></div>
-                </div>
-            </div>    
-        </div>
+            <div id="scratch-fees">
+                <p>First Row = 10 cents</p>
+                <p>Second Row = 15 cents</p>
+                <p>Third Row = 20 cents</p>
+                <p>Forth Row = 25 cents</p>
+            </div>
+        </>
     );
 }
